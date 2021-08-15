c10c6092b45f7f44167cb72a16f87742d575c2b2
philip wester <phwe@kth.se>
2021-08-15 21:52:53 +0200

    Created function to generalize NSL-KDD to binary classifications.

--- generalize_datasets.py
+++ generalize_datasets.py
@@ -1,19 +1,13 @@
 
 
-def generalize_datset(load_file_path, save_file_path, training_bool):
+def generalize_datset(load_file_path, save_file_path):
     new_dataset = []
     class_index = 41
-    # path = "res/NSL-KDD/KDDTest+.txt"
-    if training_bool:
-        dos = ["back", "land", "neptune", "pod", "smurf", "teardrop"]
-        probe = ["ipsweep", "nmap", "portsweep", "satan"]
-        r2l = ["spy", "warezclient", "ftp_write", "guess_passwd", "imap", "multihop", "phf", "warezmaster"]
-        u2r = ["buffer_overflow", "ps", "loadmodule", "rootkit", "perl"]
-    else:
-        dos = ["apache2", "back", "land", "mailbomb", "neptune", "pod", "smurf", "teardrop", "worm", "processtable", "udpstorm"]
-        probe = ["ipsweep", "mscan", "nmap", "portsweep", "saint", "satan"]
-        r2l = ["ftp_write", "guess_passwd", "httptunnel", "imap", "multihop", "named", "phf", "sendmail", "snmpgetattack", "snmpguess", "xlock", "warezmaster", "xsnoop"]
-        u2r = ["buffer_overflow", "ps", "perl", "loadmodule", "sqlattack", "xterm", "rootkit"]
+    dos = ["back", "land", "neptune", "pod", "smurf", "teardrop", "apache2", "back", "land", "mailbomb", "neptune", "pod", "smurf", "teardrop", "worm", "processtable", "udpstorm"]
+    probe = ["ipsweep", "mscan", "nmap", "portsweep", "saint", "satan"] # ["ipsweep", "nmap", "portsweep", "satan"]
+    r2l = ["spy", "warezclient", "ftp_write", "guess_passwd", "imap", "multihop", "phf", "warezmaster", "ftp_write", "guess_passwd", "httptunnel", "imap", "multihop", "named", "phf", "sendmail", "snmpgetattack", "snmpguess", "xlock", "warezmaster", "xsnoop"]
+    u2r = ["buffer_overflow", "ps", "loadmodule", "rootkit", "perl", "buffer_overflow", "ps", "perl", "loadmodule", "sqlattack", "xterm", "rootkit"]
+
     with open(load_file_path, 'r') as file:
         for line in file:
             data_row = line.split(',')
@@ -34,8 +28,25 @@ def generalize_datset(load_file_path, save_file_path, training_bool):
     with open(save_file_path, 'w') as file:
         for data_row in new_dataset:
             text_row = ','.join(str(elem) for elem in data_row)
-            # print(text_row)
             file.write(text_row)
 
 
-generalize_datset("res/NSL-KDD/KDDTrain+.txt", "res/mod_NSL/KDDTrain+.txt", True)
+def make_binary(load_file_path, save_file_path):
+    new_dataset = []
+    class_index = 41
+
+    with open(load_file_path, 'r') as file:
+        for line in file:
+            data_row = line.split(',')
+            if data_row[class_index] != "normal":
+                data_row[class_index] = "anomaly"
+            new_dataset.append(data_row)
+
+    with open(save_file_path, 'w') as file:
+        for data_row in new_dataset:
+            text_row = ','.join(str(elem) for elem in data_row)
+            file.write(text_row)
+
+generalize_datset("res/NSL-KDD/KDDTrain+.txt", "res/mod_NSL/mod_5/KDDTrain+.txt")
+
+#   make_binary("res/NSL-KDD/KDDTest+.txt", "res/mod_NSL/KDDTest+.txt")
